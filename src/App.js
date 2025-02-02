d220dddadd238c3ee55afa4358b37f5c0e8be168
Simo Kemppinen <skemppinen@outlook.com>
2025-02-02 09:54:05 +0200

    muutokset talteen

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component'
 
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "Asiakkaat",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Loukut",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Raportit",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Syötit",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Muut",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
   return (
-    <Directory categories={categories}/>
-  );
+    <Routes>
+      <Route path='/home' element={<Home />} />
+    </Routes>
+  )
 };
 
 export default App;
