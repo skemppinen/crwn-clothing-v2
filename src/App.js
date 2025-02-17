6675f11a4554086ee0e83d96a8d006a749aa47cb
Simo Kemppinen <skemppinen@outlook.com>
2025-02-17 08:06:00 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.component'
-import Navigation from './routes/navigation/navigation.component'
-import Authentication from './routes/authentication/authentication.component'
-
-import Asiakkaat from './routes/shop/shop.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='asiakkaat' element={<Asiakkaat />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
-  )
+  );
 };
 
 export default App;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,33 +1,29 @@
-import './product-card.styles.scss'
-import Button from '../button/button.component'
+import { useContext } from 'react';
 
+import { CartContext } from '../../contexts/cart.context';
 
-const ProductCard = ({asiakas}) => {
-    const {name, state, imageUrl, id, createdAt, longitude, latitude, batteryPercentage, batteryVolts } = asiakas;
-return (<div className='product-card-container'>
-    <img src={imageUrl} alt={`${name}`}/>
-    <div className='footer'>
-    <span className='name'>{name}</span>
+import Button from '../button/button.component';
+
+import './product-card.styles.scss';
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted' onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
-    <div>
-    <span className='state'>{state}</span>
-    </div>
-    <div className='id'>
-    <span className='id'>{id}</span>
-    </div>
-    <div className='created'>
-    <span className='createdAt'>{createdAt}</span>
-    </div>
-    <div className='location'>
-    <span className='longitude'>{longitude}</span>
-    <span className='latitude'>{latitude}</span>
-    </div>
-    <div className='battery'>
-    <span className='batteryPercentage'>{batteryPercentage}</span>
-    <span className='batteryVolts'>{batteryVolts}</span>
-    </div>
-    <Button buttonType='inverted'>Tarkastele</Button>
-</div>)
-}
+  );
+};
 
 export default ProductCard;
